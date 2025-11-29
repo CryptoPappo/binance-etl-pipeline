c07bc29ef820869210c07ab14a8b96f740068cd0
CryptoPappo <cryptopappo@protonmail.com>
2025-11-29 19:30:42 +0100

    Changed plot_candlesticks

--- src/analysis.py
+++ src/analysis.py
@@ -4,7 +4,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.engine.base import Engine
 import matplotlib.pyplot as plt
 import matplotlib.dates as mpdates
-from mplfinance.original_flavor import candlestick_ohlc
+import mplfinance as mpf
 with open("/root/binance-etl-pipeline/config/config.yaml") as f:
     config = yaml.safe_load(f)
 db_url = config["database"]["url"]
@@ -15,8 +15,7 @@ def load_db(query: str, engine: Engine) -> pd.DataFrame:
     return df
 
 def plot_candlesticks(start_time: str, end_time: str, interval: str = 'day',
-                      show: bool = True, savefig: bool = False,
-                      path: str = ""):
+                      show: bool = True, savefig: bool = False, path: str = ""):
     engine = create_engine(db_url)
     open_query = f"""
     SELECT date, price AS open
@@ -47,7 +46,7 @@ def plot_candlesticks(start_time: str, end_time: str, interval: str = 'day',
     WHERE t.row_number = 1;
     """
     close_df = pd.read_sql(close_query, engine)
-    df = pd.merge(open_df, close_df, on='date', how='inner')
+    df = pd.merge(open_df, close_df, on="date", how="inner")
 
     high_low_query = f"""
     SELECT date_trunc('{interval}', time) AS date, MIN(price) AS low, MAX(price) AS high
@@ -56,37 +55,25 @@ def plot_candlesticks(start_time: str, end_time: str, interval: str = 'day',
     GROUP BY date_trunc('{interval}', time);
     """
     high_low_df = pd.read_sql(high_low_query, engine)
-    df = pd.merge(df, high_low_df, on='date', how='inner')
-    df = df[["date", "open", "high", "low", "close"]]
-    df["date"] = df["date"].map(mpdates.date2num)
+    df = pd.merge(df, high_low_df, on="date", how="inner")
 
-    fig, ax = plt.subplots()
+    vol_query = f"""
+    SELECT date_trunc('{interval}', time) AS date, SUM(quantity) AS volume
+    FROM trades
+    WHERE time BETWEEN '{start_time}' AND '{end_time}' 
+    GROUP BY date_trunc('{interval}', time);
+    """
+    vol_df = pd.read_sql(vol_query, engine)
+    df = pd.merge(df, vol_df, on="date", how="inner")
+    df = df.set_index("date")
+    df = df[["open", "high", "low", "close", "volume"]]
     
-    width = 0.6
-    if interval == "hour":
-        width /= 24
-    elif interval == "minute":
-        width /= 24*60
-    elif interval == "second":
-        width /= 24*60*60
-
-    candlestick_ohlc(ax, df.values, width=width, colorup="green", colordown="red")
-    
-    ax.set_xlabel("Date")
-    ax.set_ylabel("Price")
-    plt.title(f"Prices between {start_time} and {end_time} \n at {interval} interval")
-
-    date_format = mpdates.DateFormatter("%Y-%m-%d")
-    ax.xaxis.set_major_formatter(date_format)
-    fig.autofmt_xdate()
-
-    fig.tight_layout()
-
-    if show:
-        plt.show()
-
     if savefig:
-        plt.savefig(f"{path}price_{start_time[:10]}_{end_time[:10]}_{interval}.pdf", 
-                    format="pdf")
+        mpf.plot(df, type="candle", style="yahoo", figsize=(14, 7), volume=True, savefig=path,
+                 title=f"Prices between {start_time} and {end_time} at {interval} interval")
+
+    if show:    
+        mpf.plot(df, type="candle", style="yahoo", figsize=(14, 7), volume=True,
+                 title=f"Prices between {start_time} and {end_time} at {interval} interval")
 
     return df
