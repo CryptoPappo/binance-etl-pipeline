45037494193b8a046f3b88c0f41d9d02cf1fd0db
Juan Cruz Pappolla <jcp12196@gmail.com>
2026-03-10 17:11:45 -0300

    Fixed sql query issue

--- sql/candlesticks.sql
+++ sql/candlesticks.sql
@@ -1,8 +1,8 @@
-SELECT 
-	date_trunc(:interval, time) 		 AS interval,
-	(ARRAY_AGG(price ORDER BY time ASC))[1]  AS open,
-	MAX(price) 				 AS high,
-	MIN(price)				 AS low,
+SELECT
+	date_trunc(:interval, time) AS interval,
+	(ARRAY_AGG(price ORDER BY time ASC))[1] AS open,
+	MAX(price) AS high,
+	MIN(price) AS low,
 	(ARRAY_AGG(price ORDER BY time DESC))[1] AS close,
 FROM trades
 WHERE time BETWEEN :start_time
--- src/dashboard/test.py
+++
@@ -1,8 +0,0 @@
-import os
-
-script_dir = os.path.dirname(os.path.abspath(__file__))
-sql_dir_ = os.path.abspath("sql/candlesticks.sql")
-sql_dir = os.path.abspath(os.path.join(script_dir, "..", ".."))
-
-print(script_dir)
-print(sql_dir_)
