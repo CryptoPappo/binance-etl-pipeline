f4aed9eda099b76bcf0541198a3ffba5a4de5909
Juan Cruz Pappolla <jcp12196@gmail.com>
2026-03-11 17:27:19 -0300

    Added time range option

--- src/dashboard/app.py
+++ src/dashboard/app.py
@@ -1,8 +1,8 @@
-import os
 import sqlalchemy as sa
 import streamlit as st
 import plotly.graph_objects as go
 import pandas as pd
+from datetime import datetime, timedelta
 
 INTERVAL_OPTIONS = {
         "Hourly": "hour",
@@ -19,9 +19,34 @@ interval_label = st.selectbox(
 
 interval = INTERVAL_OPTIONS[interval_label]
 
+RANGE_OPTIONS = {
+    "Last 24 hours": timedelta(hours=24),
+    "Last 7 days": timedelta(days=7),
+    "Last 30 days": timedelta(days=30),
+    "Last 90 days": timedelta(days=90),
+}
+
+range_label = st.selectbox(
+        "Time range", 
+        list(RANGE_OPTIONS.keys())
+        index=0
+)
+delta = RANGE_OPTIONS[range_label]
+
+end_time = datetime.utcnow()
+start_time = end_time - delta
+
+with st.expander("Custom range"):
+    start_time = st.date_input("Start date")
+    end_time = st.date_input("End date")
+
 engine = sa.create_engine(st.secrets["db_url"])
 
-def build_candles_query(interval: str) -> str:
+def build_candles_query(
+        interval: str,
+        start_time: str,
+        end_time: str
+) -> str:
     return f"""
     SELECT
         date_trunc('{interval}', time) AS time_interval,
@@ -30,16 +55,17 @@ def build_candles_query(interval: str) -> str:
         MIN(price)                               AS low,
         (ARRAY_AGG(price ORDER BY time DESC))[1] AS close
     FROM trades
+    WHERE time BETWEEN '{start_time}' AND '{end_time}'
     GROUP BY time_interval
     ORDER BY time_interval;
     """
 
 @st.cache_data(ttl=3600)
-def load_candles(interval):
-    query = build_candles_query(interval)
+def load_candles(interval, start_time, end_time):
+    query = build_candles_query(interval, start_time, end_time)
     return pd.read_sql(query, engine)
 
-df = load_candles(interval)
+df = load_candles(interval, str(start_time), str(end_time))
 
 fig = go.Figure(
         data=[
