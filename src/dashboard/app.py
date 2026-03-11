9e194c6b581f75297b35f8c112a1b8bf2d2604f5
Juan Cruz Pappolla <jcp12196@gmail.com>
2026-03-11 16:13:34 -0300

    Added interval selector

--- src/dashboard/app.py
+++ src/dashboard/app.py
@@ -4,18 +4,44 @@ import streamlit as st
 import plotly.graph_objects as go
 import pandas as pd
 
+INTERVAL_OPTIONS = {
+        "Hourly": "hour",
+        "Daily": "day",
+        "Weekly": "week",
+        "Monthly": "month",
+}
+
+interval_lable = st.selectbox(
+        "Select time interval",
+        options=list(INTERVAL_OPTIONS.keys()),
+        index=0,
+)
+
+interval = INTERVAL_OPTIONS[interval_label]
+
 engine = sa.create_engine(st.secrets["db_url"])
 
-sql_dir = os.path.abspath("sql/candlesticks.sql")
-with open(sql_dir) as f:
-    sql = f.read()
+def build_candles_query(interval: str) -> str:
+    return f"""
+    SELECT
+        date_trunc('{interval}', time) AS time_interval,
+        (ARRAY_AGG(price ORDER BY time ASC))[1]  AS open,
+        MAX(price)                               AS high,
+        MIN(price)                               AS low,
+        (ARRAY_AGG(price ORDER BY time DESC))[1] AS close,
+    FROM trades
+    WHERE time BETWEEN 2025-12-01 AND 2025-12-08
+    GROUP BY time_interval
+    ORDER BY time_interval;
+    """
 
-params = {
-        "interval": "hour",
-        "start_time": "2025-12-01 00:00:00",
-        "end_time": "2025-12-02 00:00:00"
-}
-df = pd.read_sql(sa.text(sql), engine, params=params)
+@st.cache_data(ttl=3600)
+def load_candles(interval):
+    query = build_candles_query(interval)
+    return pd.read_sql(query, engine)
+
+df = load_candles(interval)
+
 fig = go.Figure(
         data=[
             go.Candlestick(
@@ -28,4 +54,5 @@ fig = go.Figure(
         ]
 )
 
+st.subheader(f"{interval_label} Candlesticks")
 st.plotly_chart(fig)
