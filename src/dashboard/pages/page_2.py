eb64e851706a7820b217b836927e8ee12f63162e
Juan Cruz Pappolla <jcp12196@gmail.com>
2026-03-15 16:18:41 -0300

    Added absolute returns autocorrelation

--- src/dashboard/pages/page_2.py
+++ src/dashboard/pages/page_2.py
@@ -60,6 +60,7 @@ def read_trades_in_chunks(
     query = sa.text(f"""
         SELECT
             time,
+            price,
             CASE
                 WHEN order_type = 'Buy'  THEN  1
                 WHEN order_type = 'Sell' THEN -1
@@ -87,24 +88,38 @@ def load_correlations(start_time, end_time):
     autocorr_sign = np.zeros(k_max)
     autocorr_size = np.zeros(k_max)
     autocorr_cross = np.zeros(k_max)
+    autocorr_returns = np.zerors(k_max)
     counts = np.zeros(k_max)
     signs = np.empty(CHUNK_SIZE+k_max, dtype=np.float32)
     sizes = np.empty(CHUNK_SIZE+k_max, dtype=np.float32)
+    returns = np.empty(CHUNK_SIZE+k_max, dtype=np.float32)
     signs[:k_max] *= 0.0
     sizes[:k_max] *= 0.0
+    prices[:k_max] *= 0.0
     start = 0
     for chunk in read_trades_in_chunks(start_time, end_time):
         n = len(chunk)
         signs[k_max:n+k_max] = chunk["sign"].to_numpy(dtype=np.float32, copy=False)
         sizes[k_max:n+k_max] = signs[k_max:n+k_max] * chunk["quantity"].to_numpy(dtype=np.float32, copy=False)
+        returns[k_max:n+k_max-1] = np.abs(
+                np.log(
+                    np.divide(
+                        chunk["price"].to_numpy(dtype=np.float32, copy=False)[1:],
+                        chunk["price"].to_numpy(dtype=np.float32, copy=False)[:-1]
+                    )
+                )
+        )
+
         for i in range(1, k_max+1):
             autocorr_sign[i-1] += np.dot(signs[i:n+k_max], signs[:n+k_max-i])
             autocorr_size[i-1] += np.dot(sizes[i:n+k_max], sizes[:n+k_max-i])
             autocorr_cross[i-1] += np.dot(signs[i:n+k_max], sizes[:n+k_max-i])
+            autocorr_returns[i-1] += np.dot(returns[i:n+k_max-1], returns[:n+k_max-i-1])
             counts[i-1] += n + start*k_max - i
 
-        signs[:k_max] = signs[-k_max:]
-        sizes[:k_max] = sizes[-k_max:]
+        signs[:k_max] = signs[n:n+k_max]
+        sizes[:k_max] = sizes[n:n+k_max]
+        returns[:k_max] = returns[n-1:n+k_max-1]
         start = 1
 
         del chunk
@@ -114,7 +129,8 @@ def load_correlations(start_time, end_time):
                 "lag": np.arange(1, k_max+1),
                 "autocorr_sign": autocorr_sign / counts,
                 "autocorr_size": autocorr_size / counts,
-                "autocorr_cross": autocorr_cross / counts
+                "autocorr_cross": autocorr_cross / counts,
+                "autocorr_returns": autocorr_returns / counts
             }
     )
     return df
@@ -163,7 +179,6 @@ figure.update_yaxes(type="log")
 st.subheader("Trade Size Autocorrelation")
 st.plotly_chart(figure)
 
-
 figure = make_subplots()
 
 figure.add_trace(
@@ -183,4 +198,25 @@ figure.update_xaxes(type="log")
 figure.update_yaxes(type="log")
 
 st.subheader("Trade Sign-Size Cross-Correlation")
+st.plotly_chart(figure)
+
+figure = make_subplots()
+
+figure.add_trace(
+        go.Scatter(
+            x=df.lag,
+            y=df.autocorr_returns,
+            mode="markers",
+            marker_color="red"
+        )
+)
+
+figure.update(layout_xaxis_rangeslider_visible=False)
+figure.update_layout(title="BTC/USDT")
+figure.update_yaxes(title_text="Correlation")
+figure.update_xaxes(title_text="Lag")
+figure.update_xaxes(type="log")
+figure.update_yaxes(type="log")
+
+st.subheader("Absolute Returns Autocorrelation")
 st.plotly_chart(figure)
