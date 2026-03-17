3d7c13782a9c0714b6c5c12cfb736fa47b3ca78c
Juan Cruz Pappolla <jcp12196@gmail.com>
2026-03-17 17:05:34 -0300

    Added style to homepage

--- src/dashboard/app.py
+++ src/dashboard/app.py
@@ -1,9 +1,9 @@
 import streamlit as st
 
-page_0 = st.Page("pages/page_0.py", title="Homepage")
-page_1 = st.Page("pages/page_1.py", title="Market Overview")
-page_2 = st.Page("pages/page_2.py", title="Correlations")
-page_3 = st.Page("pages/page_3.py", title="Distributions")
+page_0 = st.Page("pages/page_0.py", title=":house_with_garden: Homepage")
+page_1 = st.Page("pages/page_1.py", title=":chart_with_upwards_trend: Market Overview")
+page_2 = st.Page("pages/page_2.py", title=":chart_with_downwards_trend: Correlations")
+page_3 = st.Page("pages/page_3.py", title=":bar_chart: Distributions")
 
 pg = st.navigation([page_0, page_1, page_2, page_3])
 st.set_page_config(page_title="Trades Dashboard")
--- src/dashboard/pages/page_0.py
+++ src/dashboard/pages/page_0.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("Binance Trades Analysis Dashboard")
+st.title(":bank: Binance Trades Analysis Dashboard")
 
 st.markdown("""
 Welcome to the **Binance Trades Analysis Dashboard**.
@@ -19,7 +19,7 @@ It is organized into three main sections, each focusing on a different aspect of
 The dashboard is designed to allow interactive exploration of large datasets while remaining computationally efficient.
 """)
 
-st.header("Page 1 — Market Overview & Price Dynamics", divider=True)
+st.header(":chart_with_upwards_trend: Page 1 — Market Overview & Price Dynamics", divider=True)
 
 st.markdown("""
 This page focuses on **price formation and trading activity over time**.
@@ -28,19 +28,19 @@ This page focuses on **price formation and trading activity over time**.
 Charts included:
 
 
-- **Candlestick chart with volume**
+- **Candlestick chart with volume**  
     Displays OHLC price candles for a selected time interval, together with traded volume. This provides a compact view of price evolution and liquidity.
-- **Trade volume and trade count**
-    Shows how trading activity evolves over time by plotting:
+- **Trade volume and trade count**  
+    Shows how trading activity evolves over time by plotting:  
         - Total traded volume   
-        - Number of trades per interval
-    This helps distinguish between high-volume trading and high-frequency activity.
-- **Normalized delta volume**
-    Measures the imbalance between buy and sell volume, normalized by total volume.
-    This chart highlights periods of buying or selling pressure and potential order-flow dominance.
+        - Number of trades per interval  
+    This helps distinguish between high-volume trading and high-frequency activity.  
+- **Normalized delta volume**  
+    Measures the imbalance between buy and sell volume, normalized by total volume.  
+    This chart highlights periods of buying or selling pressure and potential order-flow dominance.  
 """)
 
-st.header("Page 2 — Correlations & Order Flow", divider=True)
+st.header(":chart_with_downwards_trend: Page 2 — Correlations & Order Flow", divider="red")
 
 st.markdown("""
 This section explores **orders dependencies** in trading behavior. Most of the correlations studied follow **an exponential function, a power law or a combination of both**.
@@ -49,19 +49,20 @@ This section explores **orders dependencies** in trading behavior. Most of the c
 **Charts included**:
 
 
-- **Trade sign autocorrelation**
-    Measures persistence in buy/sell order flow over orders, revealing long-memory effects in trade direction.
-- **Trade size autocorrelation**
-    Shows whether large or small trades tend to cluster.
-- **Trade sign–size cross-correlation
-    Examines the relationship between trade direction and trade size, providing insight into asymmetric trading behavior.
-- **Absolute returns autocorrelation**
-    Captures volatility clustering by measuring correlations in absolute price returns.
+- **Trade sign autocorrelation**  
+    Measures persistence in buy/sell order flow over orders, revealing long-memory effects in trade direction.  
+- **Trade size autocorrelation**  
+    Shows whether large or small trades tend to cluster.  
+- **Trade sign–size cross-correlation**  
+    Examines the relationship between trade direction and trade size, providing insight into asymmetric trading behavior.  
+- **Absolute returns autocorrelation**  
+    Captures volatility clustering by measuring correlations in absolute price returns.  
+
 
 Together, these plots characterize the market’s microstructure beyond simple price movements.
 """)
 
-st.header("Page 3 — Statistical Distributions")
+st.header(":bar_chart: Page 3 — Statistical Distributions", divider="green")
 
 st.markdown("""
 This page focuses on **distributional properties** of trades and price changes.
@@ -69,12 +70,13 @@ This page focuses on **distributional properties** of trades and price changes.
 
 **Charts included**:
 
-- **Inter-trade time histogram**
-    Distribution of time differences between consecutive trades, highlighting bursty and non-Poisson trading behavior.
-- **Trade size histogram**
-    Shows the distribution of trade sizes, typically heavy-tailed and informative about liquidity and market impact.
-- **Absolute returns histogram**
-    Displays the distribution of price changes, useful for identifying fat tails and potential power-law behavior.
+
+- **Inter-trade time histogram**  
+    Distribution of time differences between consecutive trades, highlighting bursty and non-Poisson trading behavior.  
+- **Trade size histogram**  
+    Shows the distribution of trade sizes, typically heavy-tailed and informative about liquidity and market impact.  
+- **Absolute returns histogram**  
+    Displays the distribution of price changes, useful for identifying fat tails and potential power-law behavior.  
 
 
 Logarithmic scaling is provided where appropriate to better visualize tail behavior.
